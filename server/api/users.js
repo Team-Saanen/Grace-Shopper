3a903422399295b7df9a522bfc415057cb4e9f70
calvinxhe <herb.builder@gmail.com>
2023-07-19 17:45:00 -0400

    FEATURE: routes for admin, user, jwt for admin

--- server/api/users.js
+++ server/api/users.js
@@ -1,17 +1,122 @@
-const router = require('express').Router()
-const { models: { User }} = require('../db')
-module.exports = router
+const express = require('express');
+const router = express.Router();
+const { models: { User, Products, Cart, Sales }} = require('../db');
 
+//Homepage
 router.get('/', async (req, res, next) => {
-  try {
-    const users = await User.findAll({
-      // explicitly select only the id and username fields - even though
-      // users' passwords are encrypted, it won't help if we just
-      // send everything to anyone who asks!
-      attributes: ['id', 'username']
-    })
-    res.json(users)
-  } catch (err) {
-    next(err)
-  }
-})
+    try{
+        const products = await Products.findAll();
+        res.json(products);
+    }catch(error){
+        res.status(500).json({ error: error.message });
+    }
+});
+
+router.get('/:productId', async(req, res, next) =>{
+    try {
+        const product = await Products.findByPk(req.params.productId);
+        if(productId){
+            res.json(user);
+        }else {
+            res.status(404).send('Product not found')
+        }
+    }catch (error) {
+        next(error);
+    }
+});
+//Only use this route if user is logged in
+router.put('/cart/:productId', async (req, res, next) => {
+    try {
+      const productId = req.params.productId;
+      const quantity = req.body.quantity;
+      const userId = req.user.id;
+  
+      let cart = await Cart.findOne({ where: { userId } });
+  
+      // If the cart doesn't exist, create a new cart entry for the user
+      if (!cart) {
+        cart = await Cart.create({
+          userId,
+          productIds: [productId],
+          quantities: [quantity]
+        });
+  
+        return res.json({ message: 'Product added to cart successfully' });
+      }
+  
+      // Check if the productId already exists in the productIds array
+      const productIndex = cart.productIds.indexOf(productId);
+  
+      if (productIndex !== -1) {
+        // If the productId already exists, update the corresponding quantity
+        cart.quantities[productIndex] += quantity;
+      } else {
+        // If the productId is not found, add it to the arrays
+        cart.productIds.push(productId);
+        cart.quantities.push(quantity);
+      }
+  
+      // Save the updated cart
+      await cart.save();
+  
+      res.json({ message: 'Product added to cart successfully' });
+    } catch (error) {
+      next(error);
+    }
+  });
+
+  router.post('/signup', async (req, res, next) => {
+    try {
+      const { username, password } = req.body;
+  
+      // Check if the username already exists
+      const existingUser = await User.findOne({ where: { username } });
+      if (existingUser) {
+        return res.status(409).json({ error: 'Username already exists' });
+      }
+  
+      // Create a new user
+      const newUser = await User.create({
+        username,
+        password,
+        email
+      });
+  
+      res.status(201).json({ message: 'User account created successfully', userId: newUser.id });
+    } catch (error) {
+      next(error);
+    }
+  });
+
+  router.post('/checkout', async (req, res, next) => {
+    try {
+      const userId = req.user.id;
+      const cart = await Cart.findOne({ where: { userId } });
+  
+      if (!cart) {
+        return res.status(404).json({ error: 'Cart not found' });
+      }
+  
+      // Retrieve the productIds, quantities, and calculate the total price
+      const { productIds, quantities } = cart;
+      const totalPrice = calculateTotalPrice(productIds, quantities);
+  
+      // Create a new sales entry
+      const newSale = await Sales.create({
+        userId,
+        quantities,
+        items: productIds,
+        totalPrice,
+        dateTime: new Date()
+      });
+  
+      // Clear the user's cart after successful purchase
+      await cart.destroy();
+  
+      res.status(201).json({ message: 'Purchase completed successfully', saleId: newSale.id });
+    } catch (error) {
+      next(error);
+    }
+  });
+  
+module.exports = router;
--- server/auth/index.js
+++ server/auth/index.js
@@ -1,6 +1,25 @@
 const router = require('express').Router()
 const { models: {User }} = require('../db')
 module.exports = router
+const jwt = requre('jsonwebtoken')
+
+// jwt to verify if a web client is a) logged in, and
+// b) has privilages to view all user's as an admin
+// MAKE SURE THIS FILE IS KEPT AS A SECRET!!!!!!!!!!!!!
+
+export const verifyToken = (req, res, next) =>{
+  const token = req.headers.authorization || req.query.token;
+
+  if(!token){
+    return res.status(401).json({error: 'No token provided'})
+  }
+  try {
+    const decoded = jwt.verity(token, "MHcCAQEEIDx1cX68XeH4WjsSbDu9YFyWWe43tV32BbEBCQP4etl2oAoGCCqGSM49AwEHoUQDQgAEYMxAhruGCsZKPdkPeY2RdKyiCnD0FaSw+FP16qPF6/MpxAOuJd9f2Qp3k2fs8xk8asFK4IxpcUtF1e30TmrQSg==")
+    req.user = decoded;
+  }catch(err){
+    return res.status(401).json({ error: 'Invalid token' });
+  }
+}
 
 router.post('/login', async (req, res, next) => {
   try {
