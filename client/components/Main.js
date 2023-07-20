65a1c4cf055693b26b005a42e769b56da54da51f
calvinxhe <herb.builder@gmail.com>
2023-07-20 11:46:18 -0400

    added react components to replace hardcoded html

---
+++ client/components/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <>
+      <div id="title">
+        <h1>Aloe There A</h1>
+      </div>
+    </>
+  );
+};
+
+export default Main;
