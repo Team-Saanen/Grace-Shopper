1167092694ca43360fbb986da3414537efae18e2
imaJackal <130982217+imaJackal@users.noreply.github.com>
2023-07-21 13:16:57 -0700

    Added components AllProducts SingleProduct Main

--- client/components/Main.js
+++ client/components/Main.js
@@ -1,11 +1,17 @@
 import React from "react";
+import Navbar from "./Navbar";
+import AllProducts from "./AllProducts";
 
 const Main = () => {
   return (
     <>
+      <header>
+        <Navbar />
+      </header>
       <div id="title">
         <h1>Aloe There A</h1>
       </div>
+      <AllProducts />
     </>
   );
 };
---
+++ client/components/SingleProduct.js
@@ -0,0 +1,27 @@
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useParams } from "react-router-dom";
+import { fetchSingleProduct } from "../store/productSlice";
+
+const SingleProduct = () => {
+  const dispatch = useDispatch();
+  const product = useSelector((state) => state.product);
+  const { id } = useParams();
+  useEffect(() => {
+    dispatch(fetchSingleProduct(id));
+  }, []);
+
+  return (
+    <>
+      <div>
+        <h1>Plant Info</h1>
+      </div>
+      <h2>{product.productName}</h2>
+      <img>{product.productImg}</img>
+      <p>{product.description}</p>
+      <p>{product.price}</p>
+    </>
+  );
+};
+
+export default SingleProduct;
