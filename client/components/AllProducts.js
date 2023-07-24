0df1cd0c34e4eb4264dc91a4f6f275a86bcfbfde
imaJackal <130982217+imaJackal@users.noreply.github.com>
2023-07-24 11:46:15 -0700

    front page is rendering

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchAllProducts, selectProducts } from "../store/productsSlice";
 import SingleProduct from "./SingleProduct";
@@ -8,22 +8,30 @@ const AllProducts = () => {
 
   // const products = useSelector((state) => state.products);
   const products = useSelector(selectProducts);
-  
+
   useEffect(() => {
     dispatch(fetchAllProducts());
   }, []);
-  
-  return (
-    <>
-      <h1 id="all-plants">All Plants</h1>
-      <div className="list-container">
-        <p>What if prod list?</p>
-        {/* {products.map((product) => (
-          <SingleProduct key={product.id} product={product} />
-        ))} */}
-      </div>
-    </>
-  );
+
+  if (products.length > 0) {
+    return (
+      <>
+        <h1 id="all-plants">All Plants</h1>
+        <div class="list-container">
+          {products.map((product) => (
+            <p>{product.productName}</p>
+            // <div className="product">
+            //   <h3>{product.name}</h3>
+            //   <p>Price: ${product.price}</p>
+            //   <button onClick={addToCart}>Add to Cart</button>
+            // </div>
+          ))}
+        </div>
+      </>
+    );
+  } else {
+    return <div>loading</div>;
+  }
 };
 
 export default AllProducts;
