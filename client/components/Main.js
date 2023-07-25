82fa8f953582f2be814a43d903d9309fc853eb24
Joaquin Maguire <thejmags@gmail.com>
2023-07-25 07:58:41 -0700

    Routing experiments

--- client/components/Main.js
+++ client/components/Main.js
@@ -2,17 +2,23 @@ import React from "react";
 import Navbar from "./Navbar";
 import AllProducts from "./AllProducts";
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+
 const Main = () => {
   return (
-    <>
+    <Router>
       <header>
         <Navbar />
       </header>
       <div id="title">
         <h1>Aloe There A</h1>
       </div>
+      {/* <Routes>
+        <Route path="/" element={<AllProducts />} />
+      </Routes> */}
       <AllProducts />
-    </>
+    </Router>
   );
 };
 
